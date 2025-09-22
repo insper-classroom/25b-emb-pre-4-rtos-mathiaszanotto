e781266f76fe28dd260835086ad973aca20c69d9
mathiaszanotto <mathiassz@al.insper.edu.br>
2025-09-22 13:52:35 -0300

    ex3.4

--- exe3/main.c
+++ exe3/main.c
@@ -34,12 +34,13 @@ void gpio_btn_isr(uint gpio, uint32_t events) {
     portYIELD_FROM_ISR(hpw);
 }
 
-led_1_task(void *p) {
+void led_1_task(void *p) {
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     int delay = 0;
-    while (true) {
+    for (;;) {
         if (xQueueReceive(xQueueButId, &delay, 0)) {
             printf("%d\n", delay);
         }
@@ -58,9 +59,10 @@ led_1_task(void *p) {
 void led_2_task(void *p) {
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_put(LED_PIN_G, 0);
 
     int delay = 0;
-    while (true) {
+    for (;;) {
         if (xQueueReceive(xQueueBtn2, &delay, 0)) {
             printf("%d\n", delay);
         }
@@ -77,24 +79,14 @@ void led_2_task(void *p) {
 }
 
 void btn_1_task(void *p) {
-    gpio_init(BTN_PIN_R);
-    gpio_set_dir(BTN_PIN_R, GPIO_IN);
-    gpio_pull_up(BTN_PIN_R);
-
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R,
-        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, gpio_btn_isr);
-    gpio_set_irq_enabled(BTN_PIN_G,
-        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
-
     int delay = 0;
     bool pressed = false;
     uint8_t ev;
 
     for (;;) {
         if (xQueueReceive(xQueueEdgeR, &ev, portMAX_DELAY)) {
-            if (ev == 1) {
-                pressed = true;
-            } else if (ev == 2 && pressed) {
+            if (ev == 1) pressed = true;
+            else if (ev == 2 && pressed) {
                 pressed = false;
                 delay = (delay < 1000) ? delay + 100 : 100;
                 printf("delay btn R %d \n", delay);
@@ -111,9 +103,8 @@ void btn_2_task(void *p) {
 
     for (;;) {
         if (xQueueReceive(xQueueEdgeG, &ev, portMAX_DELAY)) {
-            if (ev == 1) {
-                pressed = true;
-            } else if (ev == 2 && pressed) {
+            if (ev == 1) pressed = true;
+            else if (ev == 2 && pressed) {
                 pressed = false;
                 delay = (delay < 1000) ? delay + 100 : 100;
                 printf("delay btn G %d \n", delay);
@@ -123,19 +114,30 @@ void btn_2_task(void *p) {
     }
 }
 
-int main(void) {
+int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
+    gpio_init(LED_PIN_R); gpio_set_dir(LED_PIN_R, GPIO_OUT); gpio_put(LED_PIN_R, 0);
+    gpio_init(LED_PIN_G); gpio_set_dir(LED_PIN_G, GPIO_OUT); gpio_put(LED_PIN_G, 0);
+
+    gpio_init(BTN_PIN_R); gpio_set_dir(BTN_PIN_R, GPIO_IN); gpio_pull_up(BTN_PIN_R);
+    gpio_init(BTN_PIN_G); gpio_set_dir(BTN_PIN_G, GPIO_IN); gpio_pull_up(BTN_PIN_G);
+
     xQueueButId  = xQueueCreate(32, sizeof(int));
     xQueueBtn2   = xQueueCreate(32, sizeof(int));
-    xQueueEdgeR  = xQueueCreate(8, sizeof(uint8_t));
-    xQueueEdgeG  = xQueueCreate(8, sizeof(uint8_t));
+    xQueueEdgeR  = xQueueCreate(8,  sizeof(uint8_t));
+    xQueueEdgeG  = xQueueCreate(8,  sizeof(uint8_t));
 
-    xTaskCreate(btn_1_task, "BTN_Task 1", 512, NULL, 2, NULL);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, gpio_btn_isr);
+    gpio_set_irq_enabled(BTN_PIN_G,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(btn_2_task, "BTN_Task 2", 512, NULL, 2, NULL);
+    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 2, NULL);
     xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 2, NULL);
 
     vTaskStartScheduler();
     while (true) ;
